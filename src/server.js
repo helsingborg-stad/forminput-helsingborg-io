df30f4ab146d9b997ccd573e68eb2a2cc76cb537
Samatar Mahamed <samatar.mahamed@softhouse.se>
2019-10-01 13:30:35 +0200

    Refactorise on boilerplate

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
-const config = require('config');
 const https = require('https');
 const pino = require('express-pino-logger');
 const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
+const jsonSchemaRefParser = require('json-schema-ref-parser');
 const swaggerDocument = require('../swagger/swagger.js');
 const routes = require('./components/routes');
 const logger = require('./utils/logger');
-const WebSocketServer = require('./ws.server');
+// const WebSocketServer = require('./ws.server');
 
 /**
- * Config
+ * Enviorment Variabels
  */
-const SERVER_PORT = config.get('SERVER.PORT');
-const API_BASE = '/api/v1';
+const { PORT, API_BASE } = process.env;
 
 /**
  * Init App
@@ -23,31 +24,47 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Request logging
+/**
+ * Logging
+ */
 app.use(pino({ logger }));
 
-// Add routes to the app.
-app.get('/', (req, res) => res.send('Hello World!'));
-app.use(API_BASE, routes());
+/**
+ * Routes
+ */
 
-// Swagger for documenting the api, access through localhost:xxxx/api-docs.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(routes());
 
-const server = https.createServer(app);
+/**
+ * Swagger
+ * Access documentation through localhost:xxxx/api-docs.
+ */
+
+jsonSchemaRefParser.dereference(swaggerDocument, (err, schema) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // `schema` is just a normal JavaScript object that contains your entire JSON Schema,
+    // including referenced files, combined into a single object
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(schema));
+  }
+});
 
 /**
  * Create WebSocket server
  */
-const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
+
+// const webSocketServer = new WebSocketServer(app, `${API_BASE}/ws`);
+
 
 /**
  * Start
+ * Listen on port specfied in env-file.
  */
 
-// Listen on port specfied in env-file.
-server.listen({ port: SERVER_PORT }, async () => {
-  logger.info(`Server started on port ${SERVER_PORT}`);
-  webSocketServer.start();
+const server = app.listen({ port: PORT }, async () => {
+  logger.info(`Server started on port ${PORT}`);
+  // webSocketServer.start();
 });
 
 // Export server to use it in tests.
